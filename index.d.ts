8ba891f94eeaa75372f847ac01f3300948738d66
Kostas Loupasakis <loupax@gmail.com>
2017-05-02 08:25:33 +0300

    Turned the exports to default exports

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
 declare module "zero-fill"{
-    export function zeroFill(width:number, num:number, pad?:string|number):string;
-    export function zeroFill(width:number):{(num:number, pad?:string|number):string};
+    export default function zeroFill(width:number, num:number, pad?:string|number):string;
+    export default function zeroFill(width:number):{(num:number, pad?:string|number):string};
 }
-
-
